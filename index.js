52f4efc13c41137d3ee99473b1cd482d99b9e10f
David Keim <dmk252@cornell.edu>
2023-03-06 18:10:44 -0500

    added init function

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // DEPENDENCIES
 const fs = require("fs");
 const inquirer = require('inquirer');
+const { Shape, Circle, Square, Triangle } = require("./lib/shapes.js");
 
 // DATA
 const questions = [
     {
         type: 'input',
-        message: 'What text would you like to add?',
+        message: 'What text would you like to add? (3 characters max)',
         name: 'text',
     },
     {
@@ -17,20 +18,36 @@ const questions = [
     },
     {
         type: 'input',
-        message: 'What color should be used?',
+        message: 'What color should be used? (can use a keyword or hex code)',
         name: 'color',
     },
 ];
 
 // FUNCTIONS
-function writeToFile() {
+function writeToFile(shapeData) {
     fs.writeFile();
 }
 
 // USER INTERACTIONS
 function init() {
     inquirer.prompt(questions).then((response) => {
-        writeToFile();
+        const { text, shape, color } = response;
+        let newShape;
+        switch (shape) {
+            case "Circle":
+                newShape = new Circle(text, color);
+                break;
+            case "Square":
+                newShape = new Square(text, color);
+                break;
+            case "Triangle":
+                newShape = new Triangle(text, color);
+                break;
+            default:
+                newShape = new Circle(text, color);
+                break;
+        }
+        writeToFile(newShape);
     });
 }
 
